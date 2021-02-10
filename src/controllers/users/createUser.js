c063547df18b73fbbac8bbefeeaf0c46ed1a55f6
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-10 09:36:44 +0100

    controller user

--- src/controllers/users/createUser.js
+++ src/controllers/users/createUser.js
@@ -4,5 +4,6 @@ exports.createUser = (req, res) => {
   dbModel.tbl_users.create(req.body).then((user) => {
     const message = `L'utilisateur ${req.body.first_name} ${req.body.last_name} a bien été crée.`;
     res.json({ message, data: user });
+    console.log(user.toJSON());
   });
 };
--- src/controllers/users/destroyUser.js
+++ src/controllers/users/destroyUser.js
@@ -2,17 +2,15 @@ const { dbModel } = require('../../db/sequelize');
 const User = dbModel.tbl_users;
 
 exports.destroyOneUser = (req, res) => {
-    User.findByPk(req.params.id).then(user => {
-      const userDeleted = user;
-      User.destroy({
-        where: { id: user.id }
-      })
-      .then(_ => {
-        const message = `Le pokémon avec l'identifiant n°${userDeleted.id} a bien été supprimé.`
-        res.json({message, data: userDeleted })
-      })
+  User.findByPk(req.params.id).then(user => {
+    const userDeleted = user;
+    User.destroy({
+      where: { id: user.id }
     })
-  }
-
-
-
+      .then(_ => {
+        const message = `L'utilisateur ${userDeleted.last_name} ${userDeleted.first_name} a bien été supprimé.`;
+        res.json({ message, data: userDeleted });
+        console.log(userDeleted.toJSON());
+      });
+  });
+};
