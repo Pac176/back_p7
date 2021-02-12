d1222bc6274069a1e4e4241017b46d07f1f69397
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-12 11:08:59 +0100

    debut comments

---
+++ src/routes/comments.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// const userControllers = require("../controllers/user");
+// const user = require("../middleware/validUser");
+// const limiter = require("../utils/limiter");
+
+router.get("/posts", findAllPosts); /// fonction admin
+router.get("/posts/user/:id", findAllPostsByUserId); /// fonction admin
+router.get("/posts/:id", findOnePost); /// fonction admin/user
+router.post("/posts", createPost); /// fonction admin
+router.put("/posts/:id", updatePost); /// fonction admin
+router.delete("/posts/:id", destroyOnePost); /// fonction admin
+
+module.exports = router;
