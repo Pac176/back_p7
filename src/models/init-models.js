0697dd3179c212cf489daf48ed9803c0da372a2e
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-12 10:06:55 +0100

    eslint/camelcase

--- src/models/init-models.js
+++ src/models/init-models.js
@@ -1,24 +1,24 @@
 const DataTypes = require('sequelize').DataTypes;
-const _tbl_comments = require('./tbl_comments');
-const _tbl_posts = require('./tbl_posts');
-const _tbl_users = require('./tbl_users');
+const _tblComments = require('./tblComments');
+const _tblPosts = require('./tblPosts');
+const _tblUsers = require('./tbUsers');
 
 function initModels (sequelize) {
-  const tbl_comments = _tbl_comments(sequelize, DataTypes);
-  const tbl_posts = _tbl_posts(sequelize, DataTypes);
-  const tbl_users = _tbl_users(sequelize, DataTypes);
+  const tblComments = _tblComments(sequelize, DataTypes);
+  const tblPosts = _tblPosts(sequelize, DataTypes);
+  const tblUsers = _tblUsers(sequelize, DataTypes);
 
-  tbl_comments.belongsTo(tbl_posts, { as: 'post', foreignKey: 'post_id' });
-  tbl_posts.hasMany(tbl_comments, { as: 'tbl_comments', foreignKey: 'post_id' });
-  tbl_comments.belongsTo(tbl_users, { as: 'user', foreignKey: 'user_id' });
-  tbl_users.hasMany(tbl_comments, { as: 'tbl_comments', foreignKey: 'user_id' });
-  tbl_posts.belongsTo(tbl_users, { as: 'user', foreignKey: 'user_id' });
-  tbl_users.hasMany(tbl_posts, { as: 'tbl_posts', foreignKey: 'user_id' });
+  tblComments.belongsTo(tblPosts, { as: 'post', foreignKey: 'post_id' });
+  tblPosts.hasMany(tblComments, { as: 'tblComments', foreignKey: 'post_id' });
+  tblComments.belongsTo(tblUsers, { as: 'user', foreignKey: 'user_id' });
+  tblUsers.hasMany(tblComments, { as: 'tblComments', foreignKey: 'user_id' });
+  tblPosts.belongsTo(tblUsers, { as: 'user', foreignKey: 'user_id' });
+  tblUsers.hasMany(tblPosts, { as: 'tblPosts', foreignKey: 'user_id' });
 
   return {
-    tbl_comments,
-    tbl_posts,
-    tbl_users
+    tblComments,
+    tblPosts,
+    tblUsers
   };
 }
 
