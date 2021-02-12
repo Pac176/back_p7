0f94da0d4bd11cc2e3c3f2e71571b22df381cb6c
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-12 18:47:18 +0100

    je passe le body en body.post

--- src/controllers/posts/createPost.js
+++ src/controllers/posts/createPost.js
@@ -2,7 +2,8 @@ const { dbModel } = require('../../db/sequelize');
 const Post = dbModel.tblPosts;
 
 exports.createPost = (req, res) => {
-  Post.create(req.body).then((post) => {
+  console.log(req.body);
+  Post.create(req.body.post).then((post) => {
     console.log(post);
     const message = `Le post d'id:${post.id} de l'utilisateur ${post.user_id} a bien été crée.`;
     res.json({ message, data: post });
--- src/controllers/posts/updatePost.js
+++ src/controllers/posts/updatePost.js
@@ -2,11 +2,11 @@ const { dbModel } = require('../../db/sequelize');
 const Post = dbModel.tblPosts;
 
 exports.updatePost = (req, res) => {
-  console.log(req.body);
+ 
   Post.findByPk(req.params.id)
     .then((post) => {
       const oldpost = post;
-      Post.update(req.body, { where: { id: req.params.id } })
+      Post.update(req.body.post, { where: { id: req.params.id } })
         .then((_) => {
           const message = `le post  ${oldpost.id}  a bien été modifié`;
           res.json({ message, data: req.body });
