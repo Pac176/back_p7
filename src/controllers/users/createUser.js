9cebe72aa1ceff973a17d3a7a533123345e30cea
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-15 10:28:03 +0100

    asyncpostuser

--- src/controllers/users/createUser.js
+++ src/controllers/users/createUser.js
@@ -1,10 +1,11 @@
 const { dbModel } = require('../../db/sequelize');
 const User = dbModel.tblUsers;
 
-exports.createUser = (req, res) => {
-  User.create(req.body).then((user) => {
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
     const message = `L'utilisateur ${req.body.first_name} ${req.body.last_name} a bien été crée.`;
-    res.json({ message, data: user });
-    console.log(user.toJSON());
-  });
+    res.json({ message, data: newUser });
+    console.log(newUser.toJSON());
+  } catch {}
 };
