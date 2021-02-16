caf583fedfbe06ec0a56121c706c1bfbac0f32da
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-16 17:46:10 +0100

    createpost / destroy post

--- src/controllers/posts/createPost.js
+++ src/controllers/posts/createPost.js
@@ -4,21 +4,21 @@ const Post = dbModel.tblPosts;
 
 exports.createPost = async (req, res) => {
   
-  if (req.body.post) {
+   if (req.body.post) { 
     try {
       const newPost = await Post.create(req.body.post);
       const message = `Le post d'id:${newPost.id} de l'utilisateur ${newPost.user_id} a bien été crée.`;
       res.json({ message, data: newPost });
-      console.log(post.toJSON());
+      console.log(newPost.toJSON());
     } catch (error) {
-    console.log(error.message);
-    return res
-      .status(httpStatus.BAD_REQUEST)
-      .json({ error: 'la requete a échouée' });
-  }
-} else {
+      console.log(error.message);
       return res
         .status(httpStatus.BAD_REQUEST)
-        .json({ error: "la requete n'est pas conforme" });
+        .json({ error: 'la requete a échouée' });
     }
+  }  else {
+    return res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ error: "la requete n'est pas conforme" });
+  } 
 };
--- src/controllers/posts/destroyPost.js
+++ src/controllers/posts/destroyPost.js
@@ -4,14 +4,22 @@ const Post = dbModel.tblPosts;
 const User = dbModel.tblUsers;
 
 exports.destroyOnePost = async (req, res) => {
-  const user = await User.findByPk(req.params.id);
-  const postDeleted = await Post.findByPk(req.params.id);
-  await Post.destroy({
-    where: {
-      id: postDeleted.id
-    }
-  });
-  const message = `Le post d'id:${postDeleted.id} de l'utilisateur ${user.dataValues.first_name} ${user.dataValues.last_name} a bien été supprimé.`;
-  res.json({ message, data: postDeleted });
-  console.log(postDeleted.toJSON());
+  try {
+    const postDeleted = await Post.findByPk(req.params.id);
+    const user = await User.findByPk(postDeleted.user_id);
+    await Post.destroy({
+      where: {
+        id: postDeleted.id
+      }
+    });
+    ////question pourquoi il y a des datavalues
+    const message = `Le post d'id:${postDeleted.id} de l'utilisateur ${user.dataValues.first_name} ${user.dataValues.last_name} a bien été supprimé.`;
+    res.json({ message, data: postDeleted });
+    console.log(postDeleted.toJSON());
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(httpStatus.BAD_REQUEST)
+      .json({ error: "le post est deja supprimé ou n'existe pas" });
+  }
 };
