cf31f4d499aeb02c4ae5b651aa6658d9befebdd2
Godbille Pascal <pascal.godbille@orange.fr>
2021-02-18 09:06:22 +0100

    createpost

--- src/controllers/posts/createPost.js
+++ src/controllers/posts/createPost.js
@@ -3,22 +3,22 @@ const { dbModel } = require('../../db/sequelize');
 const Post = dbModel.tblPosts;
 
 exports.createPost = async (req, res) => {
-  
-   if (req.body.post) { 
+  if (req.body.post) {
     try {
       const newPost = await Post.create(req.body.post);
       const message = `Le post d'id:${newPost.id} de l'utilisateur ${newPost.user_id} a bien été crée.`;
-      res.json({ message, data: newPost });
       console.log(newPost.toJSON());
+      return res
+        .json({ message, data: newPost });
     } catch (error) {
       console.log(error.message);
       return res
         .status(httpStatus.BAD_REQUEST)
         .json({ error: 'la requete a échouée' });
     }
-  }  else {
+  } else {
     return res
       .status(httpStatus.BAD_REQUEST)
       .json({ error: "la requete n'est pas conforme" });
-  } 
+  }
 };
