cec32ef70d58fb7872475ba1fb01f5a2e2697cc5
Godbille Pascal <pascal.godbille@orange.fr>
2021-03-29 19:48:34 +0200

    updateuser

--- src/controllers/users/updateUser.js
+++ src/controllers/users/updateUser.js
@@ -24,9 +24,9 @@ exports.updateUser = async (req, res) => {
           .status(httpStatus.BAD_REQUEST)
           .json({ message: 'Un utilisateur a deja cet email' });
       } else {
-        const oldUser = await User.findByPk(req.params.id);
+        //const oldUser = await User.findByPk(req.params.id);
         await User.update(updateUser, { where: { id: req.params.id } });
-        const message = `l'utilisateur ${oldUser.last_name} ${oldUser.first_name} a bien été modifié`;
+        const message = 'Vos données ont été mises a jour!!!';
         // console.log(oldUser.toJSON());
         return res.json({ message, data: updateUser });
       }
@@ -34,10 +34,10 @@ exports.updateUser = async (req, res) => {
       console.log(error);
       return res
         .status(httpStatus.EXPECTATION_FAILED)
-        .json({ message: error.message });
+        .json({message: "Vérifiez les champs non valides!!"});
     }
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
     return res
       .status(httpStatus.BAD_REQUEST)
       .json({
