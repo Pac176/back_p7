6049eb4f8ddb636b9860d9bc4f00e1e3dc61ef66
Godbille Pascal <pascal.godbille@orange.fr>
2021-03-29 18:13:37 +0200

    amelioration updateuser sur email deja existant

--- src/controllers/users/updateUser.js
+++ src/controllers/users/updateUser.js
@@ -2,23 +2,40 @@ const httpStatus = require('http-status');
 const { dbModel } = require('../../db/sequelize');
 const User = dbModel.tblUsers;
 const crypto = require('../../middleware/crypto');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 exports.updateUser = async (req, res) => {
+  console.log(parseInt(req.params.id));
   try {
     const emailCrypt = crypto.encrypt(req.body.email);
-    const hash = await bcrypt.hash(req.body.password, 10);
+    // const hash = await bcrypt.hash(req.body.password, 10);
     const updateUser = {
       ...req.body,
-      email: emailCrypt,
-      password: hash
+      email: emailCrypt
+      // password: hash
     };
-    const oldUser = await User.findByPk(req.params.id);
-    await User.update(updateUser, { where: { id: req.params.id } });
-    const message = `l'utilisateur ${oldUser.last_name} ${oldUser.first_name} a bien été modifié`;
-    console.log(oldUser.toJSON());
-    return res
-      .json({ message, data: updateUser });
+    try {
+      const signUser = await User.findOne({
+        where: { email: emailCrypt }
+      });
+
+      if (signUser && (parseInt(req.params.id) !== signUser.id)) {
+        return res
+          .status(httpStatus.BAD_REQUEST)
+          .json({ message: 'Un utilisateur a deja cet email' });
+      } else {
+        const oldUser = await User.findByPk(req.params.id);
+        await User.update(updateUser, { where: { id: req.params.id } });
+        const message = `l'utilisateur ${oldUser.last_name} ${oldUser.first_name} a bien été modifié`;
+        // console.log(oldUser.toJSON());
+        return res.json({ message, data: updateUser });
+      }
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(httpStatus.EXPECTATION_FAILED)
+        .json({ message: error.message });
+    }
   } catch (error) {
     console.log(error.message);
     return res
