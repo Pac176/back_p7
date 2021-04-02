4f99a459aacfadff0e7be23949ff571ab2bd0171
Godbille Pascal <pascal.godbille@orange.fr>
2021-04-02 14:45:58 +0200

    middleware d'update

--- src/middleware/validUsers.js
+++ src/middleware/validUsers.js
@@ -58,3 +58,33 @@ exports.userLoginValidation = async (req, res, next) => {
     });
   }
 };
+
+exports.userUpdateValidation = async (req, res, next) => {
+  const joiSchema = Joi.object({
+    first_name: Joi.string().min(2).max(50).alphanum(),
+    last_name: Joi.string().min(2).max(50).alphanum(),
+    pseudo: Joi.string().allow(''),
+    email: Joi.string()
+      .regex(/^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,6}$/),
+    password: Joi.string()
+      .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+      .min(8)
+
+  });
+  try {
+    await joiSchema.validateAsync(req.body, {
+      abortEarly: false
+    });
+    next();
+  } catch (error) {
+    const errorMessage = [];
+    console.log(error.annotate());
+    error.details.forEach((element) => {
+      errorMessage.push(element.message);
+    });
+    res.status(EXPECTATION_FAILED).json({
+      message: `il y a ${errorMessage.length} champ(s) invalide(s)`,
+      detail: errorMessage
+    });
+  }
+};
