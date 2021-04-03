bf6a94d2dafeabe285edf01ad17188bcff043933
Godbille Pascal <pascal.godbille@orange.fr>
2021-04-03 14:09:43 +0200

    modif signupe pour prendre en coimpte unique true email

--- src/controllers/users/signup.js
+++ src/controllers/users/signup.js
@@ -8,29 +8,25 @@ exports.signup = async (req, res, next) => {
   try {
     const emailCrypt = crypto.encrypt(req.body.email);
     const hash = await bcrypt.hash(req.body.password, 10);
-    const newUser =
-       {
-         ...req.body,
-         email: emailCrypt,
-         password: hash
-       };
     try {
-      const signUser = await User.findOrCreate({
-        where: { email: emailCrypt },
-        defaults: newUser
+      const signUser = await User.create({
+        ...req.body,
+        email: emailCrypt,
+        password: hash
       });
-      if (!signUser[1]) { /// /la fonction findOrCreate renvoi un booleen en [1]
-        return res
-          .status(httpStatus.BAD_REQUEST).json({ message: 'Le mail a deja été enristré par un utilisateur' });
-      } else {
-        const user = await User.findOne({ where: { email: emailCrypt } });
-        return res
-          .status(httpStatus.CREATED).json({ message: 'Utilisateur créé !', userId: user.id });
-      };
-    } catch (error) {
-      console.log(error);
+      const user = await User.findOne({ where: { email: emailCrypt } });
       return res
-        .status(httpStatus.EXPECTATION_FAILED).json({ message: error.message });
+        .status(httpStatus.CREATED)
+        .json({ message: 'Utilisateur créé !', userId: user.id });
+    } catch (error) {
+      if (error.name === 'SequelizeUniqueConstraintError') {
+        return res
+          .status(httpStatus.EXPECTATION_FAILED)
+          .json({ message: 'Le mail a deja été enregistré par un utilisateur' });
+      } else {
+        return res
+          .status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: error.message });
+      }
     }
   } catch (error) {
     console.log(error);
