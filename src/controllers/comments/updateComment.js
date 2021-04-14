476bb2eec8a14c400174d1ff3215e92ff80436e8
Godbille Pascal <pascal.godbille@orange.fr>
2021-04-14 22:39:13 +0200

    updatecomment / findonepost

--- src/controllers/comments/updateComment.js
+++ src/controllers/comments/updateComment.js
@@ -4,20 +4,16 @@ const Comment = dbModel.tblComments;
 
 exports.updatecomment = async (req, res) => {
   try {
-    console.log(req);
-    const oldcomment = await Comment.findByPk(req.params.id);
+    const oldComment = await Comment.findByPk(req.params.id);
     await Comment.update(req.body.comment, { where: { id: req.params.id } });
-    const message = `le comment  ${oldcomment.id} a bien été modifié`;
-    console.log(oldcomment.toJSON());
-    return res
-      .json({ message, data: req.body.comment });
+    const message = `le comment  ${oldComment.id} a bien été modifié`;
+    console.log(oldComment.toJSON());
+    return res.json({ message, data: req.body.post });
   } catch (error) {
     console.log(error.message);
-    return res
-      .status(httpStatus.BAD_REQUEST)
-      .json({
-        error: 'la requete a échouée',
-        message: error.message
-      });
+    return res.status(httpStatus.BAD_REQUEST).json({
+      error: 'la requete a échouée',
+      message: error.message
+    });
   }
 };
